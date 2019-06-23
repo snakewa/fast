1a947097ba702b97242ba3c349e21bc00d088251
Tim <tim@wizmacau.com>
2019-06-23 15:56:45 +0800

    quick fix for ssl error: https://github.com/aio-libs/aiohttp/issues/3535#issuecomment-483268542

--- fast.py
+++ fast.py
@@ -1,5 +1,5 @@
 import re
-from asyncio import ensure_future, gather, get_event_loop, sleep
+from asyncio import ensure_future, gather, get_event_loop, sleep, get_running_loop
 from collections import deque
 from statistics import mean
 from time import time
@@ -17,6 +17,7 @@ sessions = []
 
 
 async def run():
+    ignore_aiohttp_ssl_eror(get_running_loop()), 
     print('fast.com cli')
     token = await get_token()
     urls = await get_urls(token)
@@ -28,7 +29,7 @@ async def run():
 
 
 async def get_token():
-    with ClientSession() as s:
+    async with ClientSession() as s:
         resp = await s.get('https://fast.com/')
         text = await resp.text()
         script = re.search(r'<script src="(.*?)">', text).group(1)
@@ -109,6 +110,50 @@ async def cleanup():
     await gather(*[s.close() for s in sessions])
     print()
 
+def ignore_aiohttp_ssl_eror(loop, aiohttpversion='3.5.4'):
+    """Ignore aiohttp #3535 issue with SSL data after close
+
+    There appears to be an issue on Python 3.7 and aiohttp SSL that throws a
+    ssl.SSLError fatal error (ssl.SSLError: [SSL: KRB5_S_INIT] application data
+    after close notify (_ssl.c:2609)) after we are already done with the
+    connection. See GitHub issue aio-libs/aiohttp#3535
+
+    Given a loop, this sets up a exception handler that ignores this specific
+    exception, but passes everything else on to the previous exception handler
+    this one replaces.
+
+    If the current aiohttp version is not exactly equal to aiohttpversion
+    nothing is done, assuming that the next version will have this bug fixed.
+    This can be disabled by setting this parameter to None
+
+    """
+    import ssl
+    import aiohttp
+    import asyncio
+    if aiohttpversion is not None and aiohttp.__version__ != aiohttpversion:
+        return
+
+    orig_handler = loop.get_exception_handler()
+
+    def ignore_ssl_error(loop, context):
+        if context.get('message') == 'SSL error in data received':
+            # validate we have the right exception, transport and protocol
+            exception = context.get('exception')
+            protocol = context.get('protocol')
+            if (
+                isinstance(exception, ssl.SSLError) and exception.reason == 'KRB5_S_INIT' and
+                isinstance(protocol, asyncio.sslproto.SSLProtocol) and
+                isinstance(protocol._app_protocol, aiohttp.client_proto.ResponseHandler)
+            ):
+                if loop.get_debug():
+                    asyncio.log.logger.debug('Ignoring aiohttp SSL KRB5_S_INIT error')
+                return
+        if orig_handler is not None:
+            orig_handler(loop, context)
+        else:
+            loop.default_exception_handler(context)
+
+    loop.set_exception_handler(ignore_ssl_error)
 
 def dot():
     print('.', end='', flush=True)
